b095177274199562683fd523b8773aa0a4a10624
Stefan M. Kudwien <mail@stefankudwien.com>
2015-10-29 15:17:28 +0100

    Initial commit using hardcoded demo form.

---
+++ src/Plugin/Field/FieldType/ClassierParagraphsItem.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\classier_paragraphs\Plugin\Field\FieldType\ClassierParagraphsItem.
+ */
+
+namespace Drupal\classier_paragraphs\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'classier_paragraphs' entity field type.
+ *
+ * @FieldType(
+ *   id = "classier_paragraphs",
+ *   label = @Translation("Classier Paragraphs"),
+ *   description = @Translation("An entity field for storing CSS class values."),
+ *   category = @Translation("Paragraphs"),
+ *   default_widget = "classier_paragraphs_form",
+ *   default_formatter = "basic_string"
+ * )
+ */
+class ClassierParagraphsItem extends FieldItemBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Serialized classes'));
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return array(
+      'columns' => array(
+        'value' => array(
+          'type' => 'blob',
+          'size' => 'big',
+        ),
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setValue($values, $notify = TRUE) {
+    if (isset($values['value'])) {
+      $values += unserialize($values['value']);
+    }
+    else {
+      unset($values['value']);
+      $values['value'] = serialize($values);
+    }
+    parent::setValue($values, $notify);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    if (isset($this->value)) {
+      return count(unserialize($this->value)) == 0;
+    }
+    else {
+      return count($this->values) == 0;
+    }
+  }
+}
---
+++ src/Plugin/Field/FieldWidget/ClassierParagraphsWidget.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\classier_paragraphs\Plugin\Field\FieldWidget\ClassierParagraphsWidget.
+ */
+
+namespace Drupal\classier_paragraphs\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'classier_paragraphs' widget.
+ *
+ * @FieldWidget(
+ *   id = "classier_paragraphs_form",
+ *   label = @Translation("Classier Paragraphs form"),
+ *   field_types = {
+ *     "classier_paragraphs"
+ *   }
+ * )
+ */
+class ClassierParagraphsWidget extends WidgetBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element['title'] = [
+      '#type' => 'radios',
+      '#title' => 'Ausrichtung Titel',
+      '#options' => [
+        'title_left' => 'linksbündig',
+        'title_center' => 'zentriert',
+      ],
+      '#default_value' => isset($items[$delta]->title) ? $items[$delta]->title : 'title_left',
+    ];
+    $element['background'] = [
+      '#type' => 'radios',
+      '#title' => 'Hintergrund',
+      '#options' => [
+        'background_none' => 'keiner / weiß',
+        'background_color_signature_green_light' => 'hellgrün',
+        'background_image_wood' => 'Bühne mit Holz',
+      ],
+      '#default_value' => isset($items[$delta]->background) ? $items[$delta]->background : 'background_none',
+//      '#theme_wrappers' => [
+//        'my_form_element' // for markup in #title?
+//      ]
+    ];
+    return $element;
+  }
+}
