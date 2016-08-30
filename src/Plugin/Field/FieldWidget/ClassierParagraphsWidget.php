cb62839f83220c6acb2ac4f64f4789857850bb46
Stefan M. Kudwien <mail@stefankudwien.com>
2016-08-30 15:44:51 +0200

    Replace useless $entity_type parameter (which is always 'paragraph') with the EntityFormDisplay object, which provides access to the parent entity form the paragraph is attached to (e.g. targetEntityType, targetBundle, mode).

--- src/Plugin/Field/FieldWidget/ClassierParagraphsWidget.php
+++ src/Plugin/Field/FieldWidget/ClassierParagraphsWidget.php
@@ -30,15 +30,25 @@ class ClassierParagraphsWidget extends WidgetBase {
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     /* @var $entity \Drupal\Core\Entity\FieldableEntityInterface */
     $entity = $items->getParent()->getValue();
-    return $this->subForm($entity->getEntityTypeId(), $entity->bundle(), $items[$delta]);
+    /* @var $form_display Drupal\Core\Entity\Entity\EntityFormDisplay */
+    $form_display = $form_state->getStorage()['form_display'];
+    return $this->subForm($form_display, $entity->bundle(), $items[$delta]);
   }
 
   /**
    * Builds the user-specific subform.
+   *
+   * @param Drupal\Core\Entity\Entity\EntityFormDisplay $form_display
+   *   Provides access to form display properties, including targetEntityType,
+   *   targetBundle, and display mode.
+   * @param string $bundle
+   *   The paragraph bundle to build the form for.
+   * @param Drupal\classier_paragraphs\Plugin\Field\FieldType\ClassierParagraphsItem $item
+   *   The paragraph field item.
    */
-  public function subForm($entity_type, $bundle, $item) {
+  public function subForm($form_display, $bundle, $item) {
     // Invoke hook_classier_paragraphs_form
-    $element = Drupal::moduleHandler()->invokeAll('classier_paragraphs_form', array($entity_type, $bundle, $item));
+    $element = Drupal::moduleHandler()->invokeAll('classier_paragraphs_form', array($form_display, $bundle, $item));
 
     // Let the themes play too, because classes for paragraphs is quite a themey thing.
     $theme_handler = Drupal::service('theme_handler');
@@ -50,11 +60,16 @@ class ClassierParagraphsWidget extends WidgetBase {
       $function = $theme_name . '_classier_paragraphs_form';
       $theme_handler->getTheme($theme_name)->load();
       if (function_exists($function)) {
-        $element = $function($entity_type, $bundle, $item);
+        $element = $function($form_display, $bundle, $item);
       }
     }
 
-    Drupal::moduleHandler()->alter('classier_paragraphs_form', $element, $entity_type, $bundle, $item);
+    $context = array(
+      'form_display' => $form_display,
+      'bundle' => $bundle,
+      'item' => $item
+    );
+    Drupal::moduleHandler()->alter('classier_paragraphs_form', $element, $context);
 
     return $element;
   }
